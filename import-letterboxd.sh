f7735e04f34f0371c35d4455b9b2faf8c0f8e7db
Martin Knudsen <martin.knudsen@gmail.com>
2022-04-12 19:43:36 +0200

    Update import-letterboxd.sh
    manually install additional dependency as described here https://import.jekyllrb.com/docs/rss/

--- import-letterboxd.sh
+++ import-letterboxd.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 gem install jekyll-import
+gem install safe_yaml
 ruby -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
       "source" => "https://letterboxd.com/kndsn/rss/",
