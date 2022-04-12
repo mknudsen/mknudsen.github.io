4c80a859f1910b16a9858488990dcb9e69f70d08
Martin Knudsen <martin.knudsen@gmail.com>
2022-04-12 19:41:08 +0200

    Update import-goodreads.sh
    manually install additional dependency as described here https://import.jekyllrb.com/docs/rss/

--- import-goodreads.sh
+++ import-goodreads.sh
@@ -2,6 +2,7 @@
 
 GOODREADS_FEED=$1
 gem install jekyll-import
+gem install safe_yaml
 ruby -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
       "source" => "'${GOODREADS_FEED}'",
