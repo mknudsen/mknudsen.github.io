8562e2f374433b4824b3223f844e864f0403f05f
Martin Knudsen <martin.knudsen@gmail.com>
2022-04-14 16:18:04 +0200

    Update import-letterboxd.sh

--- import-letterboxd.sh
+++ import-letterboxd.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-pwd
-ls
+set -euo pipefail
 
 gem install jekyll-import
 gem install safe_yaml
-ruby -r rubygems -e 'require "jekyll-import";
+ruby -w -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
       "source" => "https://letterboxd.com/kndsn/rss/",
       "tag" => "movies"
