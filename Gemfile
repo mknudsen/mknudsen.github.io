175e65d8c7f01c3779324adb609fae77977a22ca
Martin Knudsen <martin.knudsen@otto.de>
2022-04-14 16:23:15 +0200

    add gemfiles, switch to bundler for import-letterboxd.sh

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "jekyll", "~> 4.2"
+gem "jekyll-import", "~> 0.21.0"
+gem "minima"
--- import-letterboxd.sh
+++ import-letterboxd.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-gem install jekyll-import
-gem install safe_yaml
+bundle install
+
 ruby -w -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
       "source" => "https://letterboxd.com/kndsn/rss/",
