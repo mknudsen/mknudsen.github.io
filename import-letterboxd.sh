83c131a7d2704b75debfc8b8e85c3ab54f0f2a0d
Martin Knudsen <martin.knudsen@otto.de>
2022-04-14 16:30:36 +0200

    patch open() in rss.rb

--- import-letterboxd.sh
+++ import-letterboxd.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 bundle install
 
+sed 's#open(source)#URI.open(source)#' "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
+cat "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
+
 ruby -w -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
       "source" => "https://letterboxd.com/kndsn/rss/",
