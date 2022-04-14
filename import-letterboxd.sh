38357dfb2f504bdae99dcf250c3b78c66fc0ed40
Martin Knudsen <martin.knudsen@gmail.com>
2022-04-14 16:53:59 +0200

    Update import-letterboxd.sh

--- import-letterboxd.sh
+++ import-letterboxd.sh
@@ -2,10 +2,14 @@
 
 set -euo pipefail
 
-bundle install
+# bundle install
 
-sed 's#open(source)#URI.open(source)#' "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
-cat "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
+#sed 's#open(source)#URI.open(source)#' "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
+#cat "$(bundle show jekyll-import)"/lib/jekyll-import/importers/rss.rb
+
+curl -v "https://letterboxd.com/kndsn/rss/" 
+
+exit 0
 
 ruby -w -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
