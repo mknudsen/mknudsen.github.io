d5985a7952bbd7f7b0a95435370c76f6dc02aaff
Martin Knudsen <martin.knudsen@gmail.com>
2023-04-16 15:59:18 +0200

    Update import-goodreads.sh

--- import-goodreads.sh
+++ import-goodreads.sh
@@ -8,6 +8,8 @@ bundle install
 
 # for some reason setting "source" => '$GOODREADS_FEED' fails at retrieving in
 # github actions although it works locally so I download the feed manually
+apk update
+apk upgrade
 apk add curl
 GOODREADS_FEED=$1
 curl -o goodreads.xml "$GOODREADS_FEED"
