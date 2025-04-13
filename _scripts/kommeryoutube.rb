867b59cb9f4a82cc2604da8a40d57b35efd7f43c
Martin Knudsen <martin.knudsen@otto.de>
2025-04-13 14:36:28 +0200

    add kommer videos

---
+++ _scripts/kommeryoutube.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+
+CHANNEL_URL = "https://www.youtube.com/@gerdkommer/videos"
+DATA_FILE = "_data/kommer_youtube.yml"
+
+def fetch_youtube_videos(channel_url)
+  command = "yt-dlp --flat-playlist -J #{channel_url}"
+  output = `#{command}`
+  json_data = JSON.parse(output)
+
+  videos = json_data["entries"].map do |video|
+    {
+      "title" => video["title"],
+      "link"   => "https://www.youtube.com/watch?v=#{video['id']}"
+    }
+  end.sort_by { |entry| entry["title"] }
+
+  File.open(DATA_FILE, "w") { |file| file.write(videos.to_yaml) }
+  puts "Saved #{videos.size} videos to #{DATA_FILE}"
+end
+
+fetch_youtube_videos(CHANNEL_URL)
